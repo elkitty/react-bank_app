a45f49cfffc52897da09f9539aaa4ade1d85c82d
elkitty <hablaconwafle@gmail.com>
2023-11-29 16:53:23 -0300

    commit inicial

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import styles from '../style';
+import discount from '../assets/discount.svg';
+import robot from '../assets/robot.png';
+
+
+
+const Hero = () => {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
